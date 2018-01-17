d4067b046e801e954755cbd00936c873bb5f7d8c
Chris Kennedy <chrisken@gmail.com>
2018-01-17 14:02:47 -0800

    Add deprecated aliases for old function names: ittatetmle, catetmle, and eatetmle

---
+++ R/aliases.R
@@ -0,0 +1,23 @@
+#' Deprecated, please update code.
+#' @param ... All arguments passed through to new function.
+#' @export
+ittatetmle = function(...) {
+  .Deprecated("transport_ittate", "transport")
+  transport_ittate(...)
+}
+
+#' Deprecated, please update code.
+#' @param ... All arguments passed through to new function.
+#' @export
+catetmle = function(...) {
+  .Deprecated("transport_cace", "transport")
+  transport_cace(...)
+}
+
+#' Deprecated, please update code.
+#' @param ... All arguments passed through to new function.
+#' @export
+eatetmle = function(...) {
+  .Deprecated("transport_eace", "transport")
+  transport_eace(...)
+}
